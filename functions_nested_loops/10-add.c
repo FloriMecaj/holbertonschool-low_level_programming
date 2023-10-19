f291e230956258d998b9b180860c844bbd0f992a
FloriMecaj <florimecaj31@gmail.com>
2023-10-19 05:45:04 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @first_n: -first number
+ * @second_n: second num
+ * Return the sum of two int numbers;
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
