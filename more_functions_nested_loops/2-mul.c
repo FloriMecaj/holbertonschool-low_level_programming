c2c1180022a67b923d40a1a0c03fc7d697e766a4
FloriMecaj <florimecaj31@gmail.com>
2023-10-20 01:12:44 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * main.h header file
+ * @a: is parameter first num to make multiplication
+ * @b: parameter second num to make multiplication
+ * Return:0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+	return (result);
+}
