baed34a1bb61648a8793302b8dd809531c20fd4d
FloriMecaj <florimecaj31@gmail.com>
2023-10-20 01:42:04 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * print_most_numbers - function print except 2,4
+ *   * main.h header file
+ *    * Return:0
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
