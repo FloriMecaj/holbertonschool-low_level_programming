4e7c39188ac05010aa6727f78df9d41369b2176e
FloriMecaj <florimecaj31@gmail.com>
2023-10-25 07:06:22 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,15 +14,15 @@ char *cap_string(char *str)
 	{
 		if (i == 0)
 		{
-			if (str[i+1] >= 'a' && str[i+1] <= 'z')
-				str[i+1] = str[i+1] - 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
 		}
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';'
 			|| str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '('
 			|| str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
-			 if (str[i+1] >= 'a' && str[i+1] <= 'z')
-				 str[i+1] = str[i+1] - 32;
+			 if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				 str[i + 1] = str[i + 1] - 32;
 		}
 	}
 	return (str);
