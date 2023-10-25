c721d153a093bc855eaaf6ea7b19ea691aa302c1
FloriMecaj <florimecaj31@gmail.com>
2023-10-25 03:49:25 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,22 @@
  *_strcmp - ygujh
  *@s1: seff
  *@s2: sdyu
+ *Return: sedfa
  */
 
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+	int diff = 0;
 
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		i++;
+	}
+	return (diff);
 }
