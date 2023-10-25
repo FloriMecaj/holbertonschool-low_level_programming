b95c001db461d7d78c5db05ac58de39a8d60cb13
FloriMecaj <florimecaj31@gmail.com>
2023-10-25 04:09:06 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *reverse_array - aedas
+ *@a: dscd 
+ *@n: aaca
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start, end, tmp;
+
+	start = 0;
+	end = n - 1;
+
+	while (start < end) 
+	{
+		tmp = a[start];
+		a[start] = a[end];
+		a[end] = tmp;
+		start++;
+		end--;
+	}
+}
