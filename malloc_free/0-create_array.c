41661459e56709a6d088230a8519a176a863df76
FloriMecaj <florimecaj31@gmail.com>
2023-11-06 02:05:29 -0800

    prototypes

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *create_array - adsca
+ *@size: afcsce
+ *@c: adfsac
+ *Return: adcca
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsign
+}
