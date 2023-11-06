3d73b2165f1c306ce28bbc3fd7daef1d63ccdb9d
FloriMecaj <florimecaj31@gmail.com>
2023-11-06 02:06:27 -0800

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,14 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- *create_array - adsca
- *@size: afcsce
- *@c: adfsac
- *Return: adcca
+ * create_array - sdfsdf
+ * @size:sdgsdg
+ * @c:sgsdgd
+ * Return:dfgsdg
  */
 
 char *create_array(unsigned int size, char c)
 {
-	unsign
+	unsigned int i = 0;
+	char *array = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
 }
