1eee4c2f9108a0e43cc672522033612c1fb8a3b7
FloriMecaj <florimecaj31@gmail.com>
2023-11-13 04:33:43 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,17 +6,15 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
  * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
