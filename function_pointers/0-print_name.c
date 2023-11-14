a0f44ec7e8d94debd9db5fc31fda4df3724ed2a1
FloriMecaj <florimecaj31@gmail.com>
2023-11-14 03:02:35 -0800

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+/**
+ *print_name - asafsaf
+ *@name: afaf
+ *@f:eafeaff
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL  && f != NULL)
+	{
+		f(name);
+	}
+}
