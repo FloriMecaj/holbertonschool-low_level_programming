7e2d309958288df5c32264a9608ed5c953b23b5d
FloriMecaj <florimecaj31@gmail.com>
2023-11-14 01:09:50 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,47 +1,34 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include "dog.h"
 
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
  * Return: pointer to the new dog_t
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *new_name, *new_owner;
+	dog_t *sec_dog = malloc(sizeof(dog_t));
+	char *sec_name = malloc(strlen(name) + 1);
+	char *sec_owner = malloc(strlen(owner) + 1);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (sec_dog == NULL || sec_name == NULL || sec_owner == NULL)
 	{
-		free(new_dog);
+		free(sec_dog);
+		free(sec_name);
+		free(sec_owner);
 		return (NULL);
 	}
 
-	new_name = malloc(strlen(name));
-	if (new_name == NULL)
-	{
-		free(new_name);
-		return (NULL);
-	}
-
-	new_owner = malloc(strlen(owner));
-	if (new_owner == NULL)
-	{
-		free(new_owner);
-		return (NULL);
-	}
-
-	strcpy(new_name, name);
-	strcpy(new_owner, owner);
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
-
-		return (new_dog);
+	strcpy(sec_name, name);
+	strcpy(sec_owner, owner);
+	sec_dog->name = sec_name;
+	sec_dog->age = age;
+	sec_dog->owner = sec_owner;
+	return (sec_dog);
 }
