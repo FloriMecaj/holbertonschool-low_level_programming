b39b4d11d3f5f76b7463a4347264d8f67d07c9e6
FloriMecaj <florimecaj31@gmail.com>
2023-11-17 02:10:56 -0800

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,12 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ *sum_them_all - acdcads
+ *@n: adcdc
+ *Return: sdccv
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
@@ -10,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start (fl, n);
+	va_start(fl, n);
 	for (i = 0; i < n; i ++)
 		sum += va_arg(fl, int);
 
