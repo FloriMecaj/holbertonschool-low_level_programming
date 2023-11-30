bf3c2d56a1b7fd4dcb0b981dd9d83a7ad5718703
FloriMecaj <florimecaj31@gmail.com>
2023-11-30 02:50:09 -0800

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ *
+ *
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
