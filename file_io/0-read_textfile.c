8acd36ea9a22b5997e4c350170dad76e47acf632
FloriMecaj <florimecaj31@gmail.com>
2023-12-04 14:28:50 -0800

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *read_textfile - efzdadcc
+ *@filename: sdccz
+ *@letters: sdvcdzvcsc
+ *Return: sdcdzcxc
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, reread, rewrite;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename O_RONLY);
+	if (file == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	reread = read(file, buffer, letters);
+	if (reread == -1)
+		return (0);
+	rewrite = write(STDOUT_FILENO, buffer, reread);
+	if (rewrite == -1)
+		return (0);
+	free(buffer);
+	close(file);
+	return (rewrite);
+}
