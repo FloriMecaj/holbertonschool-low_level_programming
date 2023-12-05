0b45d84fe8ee31b3cb361b774e65ce93a9bee24a
FloriMecaj <florimecaj31@gmail.com>
2023-12-05 02:03:07 -0800

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 /**
- *
- *
- *
- *
+ *create_file - aczxvzv
+ *@filename: ascszc
+ *@text_content: advzv
+ *Return: asvv`
  */
 
 int create_file(const char *filename, char *text_content)
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (written == -1)
 		return (-1);
-	
+
 	close(fd);
 	return (1);
 }
