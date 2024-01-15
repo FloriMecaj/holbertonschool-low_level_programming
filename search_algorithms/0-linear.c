5c2cff7c2cdeafcc65f25fe430781aadb3ee10ed
FloriMecaj <florimecaj31@gmail.com>
2024-01-15 11:34:59 +0100

    0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/***
+ * linear_search - dxfcgvuhbij
+ * @size: dgxfchvjbknlk
+ * @value: sdgever
+ * Return: gvjhbkj
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    int i;
+
+    if (array == NULL)
+        return (-1);
+
+    for (i = 0; i < (int)size; i++) {
+        printf ("Value checked array[%d] = [%d]\n", i, array[i]);
+
+            if (array[i] == value)
+            return (i);
+    }
+        return (-1);
+}
