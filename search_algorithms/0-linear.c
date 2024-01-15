d5c65d69e0df8ace0835f3deef3f578d66055173
FloriMecaj <florimecaj31@gmail.com>
2024-01-15 11:38:19 +0100

    betty fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-/***
+/**
  * linear_search - dxfcgvuhbij
+ * @array: advergsthdyf
  * @size: dgxfchvjbknlk
  * @value: sdgever
  * Return: gvjhbkj
@@ -9,16 +10,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+	int i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (i = 0; i < (int)size; i++) {
-        printf ("Value checked array[%d] = [%d]\n", i, array[i]);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
-            if (array[i] == value)
-            return (i);
-    }
-        return (-1);
+			if (array[i] == value)
+			return (i);
+	}
+		return (-1);
 }
